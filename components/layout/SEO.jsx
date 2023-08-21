1a4b3da681fe4448e86bef406744d52bd64f1c46
keilokimnida <tkl48leon@gmail.com>
2023-08-22 01:40:21 +0800

    [feat] add google site verification

--- components/layout/SEO.jsx
+++ components/layout/SEO.jsx
@@ -5,6 +5,10 @@ const SEO = ({ title = "Dolcent" }) => (
   <Head>
     <meta charSet="utf-8" />
     <title>{title}</title>
+    <meta
+      name="google-site-verification"
+      content="4-_73DOGpP5CLWnL8D31ck0ZwEJm5PxgSxS5vjFvtMA"
+    />
     <meta name="description" content="Make your Dollars and Cents Count" />
   </Head>
 );
