4ba81614d68952698abb75351ebe826a1860adb2
keiloktql <tkl48leon@gmail.com>
2025-01-26 15:29:31 +0800

    ios smart app banners

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { APP_STORE_LINK_ID } from "@/lib/config";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -5,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content="Make your Dollars and Cents count" />
+        <meta name="apple-itunes-app" content={`app-id=${APP_STORE_LINK_ID}`} />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="icon"
